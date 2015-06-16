bf9bdb92bc66b9ae19dc30cdc77a206d89800847
Indumathi <indu9086@gmail.com>
2015-06-16 23:15:40 +0200

    Update cmake for unit testing

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/d/Indu/Workspace/NoSQLDatabase/src
-# Build directory: /cygdrive/d/Indu/Workspace/NoSQLDatabase/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
